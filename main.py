58c622c68fcd3a8726178b6ac4c45cf99c2e92ba
Maanas Arora <maanasarora23@gmail.com>
2021-07-15 14:19:45 -0400

    implement solution

---
+++ main.py
@@ -0,0 +1,22 @@
+def get_queen_valid(n, queens, new_queen):
+    if new_queen in queens:
+        return False
+
+    for row, queen in enumerate(queens):
+        if abs(new_queen - queen) == abs(len(queens) - row):
+            return False
+
+    return True
+
+def solve(n):
+    stack = [[i] for i in reversed(range(n))]
+    while len(stack) > 0:
+        queens = stack.pop()
+        print(queens)
+
+        if len(queens) == n:
+            return [queen+1 for queen in queens]
+
+        for new_queen in range(n):
+            if get_queen_valid(n, queens, new_queen):
+                stack.append(queens + [new_queen])
